9c887fd7abc5d796dae980fb0b97e456ca89b01f
Chris Beyer <beyercorpuz@gmail.com>
2020-01-24 22:19:30 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,14 @@
 def unsafe?(speed)
+<<<<<<< HEAD
 if  speed  >60  || speed < 40
   
 return true
 else 
+=======
+if  speed  >60
+return true
+else 40 <speed > 60
+>>>>>>> 66dd607503500e9fa42bf2487b5fc6446bf55a5f
   return false
 end
 end
@@ -12,6 +18,10 @@ end
 
 def not_safe?(speed) 
 
+<<<<<<< HEAD
 speed < 40 || speed > 60 ? true: false
+=======
+speed < 40 ? "notsafe" : "safe"
+>>>>>>> 66dd607503500e9fa42bf2487b5fc6446bf55a5f
 
 end
